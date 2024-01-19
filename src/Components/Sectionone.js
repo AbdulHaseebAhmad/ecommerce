8e7b4bfa0d01a5a8c7ef868ebbb25fadd553d238
AbdulHaseebAhmad <79489830+AbdulHaseebAhmad@users.noreply.github.com>
2024-01-19 19:49:46 +0300

    This includes desktop Ready SectionOne,topNav and MainNav

--- src/Components/Sectionone.js
+++ src/Components/Sectionone.js
@@ -1,15 +1,42 @@
 import React from 'react'
 import classes from "../CSS/Sectionone.module.css";
 import MainNavbar from '../Components/MainNavbar';
-
+import TungstenOutlinedIcon from '@mui/icons-material/TungstenOutlined';
 
 export default function Sectionone() {
   return (
     <div className={classes.sectionOneOuterContainer}>
-      <div className={classes.sectionOneSlideShowContainer}>
         <MainNavbar/>
+        <div className={classes.sectionOneTextContainer}>
 
-      </div>
+                <div className={classes.textSecondaryContainer}>
+                    <h1>
+                        BEST CYCLING EXPERIENCE
+                    </h1>
+                </div>
+
+                <div className={classes.textSecondaryContainerBottom}>
+                    
+                    <div className={classes.secondaryTextDecoration}>
+                        <hr/>
+                        <TungstenOutlinedIcon sx={{width:'30px',height:'30px'}}/>
+                        <hr/>
+                    </div>
+
+                    <div className={classes.sectionOneParaGraphContainer}>
+                        <p>
+                            BECOME THE WORLD LEADER ON THE INTERNET FOR THE SPORT OF CYCLING
+                        </p>
+                    </div>
+
+                </div>
+
+                <div className={classes.buttonContainer}>
+                    <button className={classes.button}>
+                        SHOP NOW
+                    </button>
+                </div>
+        </div>
     </div>
   )
 }
